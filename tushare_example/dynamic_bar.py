d7de30fc6b3ff53bcf532c71fe5d65c9caafb96e
me <a@b.c>
2021-01-29 16:57:43 +0800

    测试

--- tushare_example/dynamic_bar.py
+++ tushare_example/dynamic_bar.py
@@ -2,14 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib.animation as animation
-
 import tushare as ts
+from datetime import timedelta,datetime
+
 pro = ts.pro_api('b7bbd6907437d1f894145e7487709d27ee220b6c3772205ef05c83fb')
 
-df = ts.get_tick_data('600703',date='2021-01-28',src='tt')
+days = 4
+now = datetime.now().date()
 
-a = df.groupby(["price"]).agg({"volume":"sum"})
+fd = now-timedelta(days=days)
 
-a.plot.bar()
-plt.xticks(rotation=90)
+ax = None
+code = "000425"
+# code = '600703'
+for d in [str(fd+timedelta(days=i)) for i in range(days+1)]:
+    df = ts.get_tick_data(code,date=d,src='tt')
+    a = df.groupby(["price"]).agg({"volume":"sum"})
+    a = a.head(90)
+    if not ax:
+        ax = a.plot()
+    a.plot(ax=ax,figsize=(15,5),legend=d)
+    plt.xticks(rotation=30)
+    plt.title(d)
 plt.show()
